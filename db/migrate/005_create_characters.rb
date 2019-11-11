53bf0b08bf07ed100327eb5453bf0b4d87ba80df
github username <github email address>
2019-11-10 18:39:37 -0800

    Done.

---
+++ db/migrate/005_create_characters.rb
@@ -0,0 +1,11 @@
+class CreateCharacters < ActiveRecord::Migration[5.1]
+    def change
+      create_table :characters do |t| #we get a block variable here for the table
+
+        t.string :name 
+        t.integer :actor_id
+        t.integer :show_id
+    
+      end
+    end
+  end
